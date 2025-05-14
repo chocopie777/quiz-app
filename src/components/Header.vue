0bdb47243b9cabecc01edd700d637888aa5767bf
chocopie777 <egoran99@gmail.com>
2025-05-14 22:29:24 +0500

    verstka quizview

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,9 @@
 
 <template>
   <header class="bg-blue-400 h-20 flex justify-center items-center">
-    <h3 class="text-white text-3xl font-bold">QUIZ APP</h3>
+    <RouterLink to="/">
+      <h3 class="text-white text-3xl font-bold">QUIZ APP</h3>
+    </RouterLink>
   </header>
 </template>
 
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -5,8 +5,18 @@
 <template>
   <div>
     <h2 class="mt-5 text-2xl font-medium text-blue-400">Category name</h2>
-    <div>
-
+    <div class="mt-5">
+      <h3 class="text-xl font-medium">Who is making the Web standards?</h3>
+      <form class="mt-5">
+        <div class="text-lg ring shadow-md cursor-pointer hover:shadow-none hover:text-white hover:bg-blue-400 p-2 rounded-md mb-5">The World Wide Web Consortium</div>
+        <div class="text-lg ring shadow-md cursor-pointer hover:shadow-none hover:text-white hover:bg-blue-400 p-2 rounded-md mb-5">The World Wide Web Consortium</div>
+        <div class="text-lg ring shadow-md cursor-pointer hover:shadow-none hover:text-white hover:bg-blue-400 p-2 rounded-md mb-5">The World Wide Web Consortium</div>
+        <div class="text-lg ring shadow-md cursor-pointer hover:shadow-none hover:text-white hover:bg-blue-400 p-2 rounded-md mb-5">The World Wide Web Consortium</div>
+        <div>
+          <button class="bg-blue-400 text-white p-2 text-lg rounded-md mr-2 w-25 cursor-pointer hover:bg-blue-500">Previous</button>
+          <button class="bg-blue-400 text-white p-2 text-lg rounded-md w-25 cursor-pointer hover:bg-blue-500">Next</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
