8a28ea70d67c7aaa3fa12f7d7cbb1b4d51bc0e28
chocopie777 <egoran99@gmail.com>
2025-05-15 14:11:09 +0500

    verstka quizview and resultview

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -6,15 +6,30 @@
   <div>
     <h2 class="mt-5 text-2xl font-medium text-blue-400">Category name</h2>
     <div class="mt-5">
+      <span class="block mb-2 text-blue-500 font-medium">Question: 1/30</span>
       <h3 class="text-xl font-medium">Who is making the Web standards?</h3>
       <form class="mt-5">
-        <div class="text-lg ring shadow-md cursor-pointer hover:shadow-none hover:text-white hover:bg-blue-400 p-2 rounded-md mb-5">The World Wide Web Consortium</div>
-        <div class="text-lg ring shadow-md cursor-pointer hover:shadow-none hover:text-white hover:bg-blue-400 p-2 rounded-md mb-5">The World Wide Web Consortium</div>
-        <div class="text-lg ring shadow-md cursor-pointer hover:shadow-none hover:text-white hover:bg-blue-400 p-2 rounded-md mb-5">The World Wide Web Consortium</div>
-        <div class="text-lg ring shadow-md cursor-pointer hover:shadow-none hover:text-white hover:bg-blue-400 p-2 rounded-md mb-5">The World Wide Web Consortium</div>
+        <label>
+          <input type="radio" class="w-[1px] h-[1px] overflow-hidden absolute [clip:rect(0,0,0,0)] peer" name="answer" id="answer1">
+          <div class="text-lg ring cursor-pointer hover:text-white hover:bg-blue-300 p-2 rounded-md mb-5 peer-checked:bg-blue-400  peer-checked:ring-0 peer-checked:text-white peer-checked:hover:bg-blue-400">The World Wide Web Consortium</div>
+        </label>
+        <label>
+          <input type="radio" class="w-[1px] h-[1px] overflow-hidden absolute [clip:rect(0,0,0,0)] peer" name="answer" id="answer1">
+          <div class="text-lg ring cursor-pointer hover:text-white hover:bg-blue-300 p-2 rounded-md mb-5 peer-checked:bg-blue-400  peer-checked:ring-0 peer-checked:text-white peer-checked:hover:bg-blue-400">The World Wide Web Consortium</div>
+        </label>
+        <label>
+          <input type="radio" class="w-[1px] h-[1px] overflow-hidden absolute [clip:rect(0,0,0,0)] peer" name="answer" id="answer1">
+          <div class="text-lg ring cursor-pointer hover:text-white hover:bg-blue-300 p-2 rounded-md mb-5 peer-checked:bg-blue-400  peer-checked:ring-0 peer-checked:text-white peer-checked:hover:bg-blue-400">The World Wide Web Consortium</div>
+        </label>
+        <label>
+          <input type="radio" class="w-[1px] h-[1px] overflow-hidden absolute [clip:rect(0,0,0,0)] peer" name="answer" id="answer1">
+          <div class="text-lg ring cursor-pointer hover:text-white hover:bg-blue-300 p-2 rounded-md mb-5 peer-checked:bg-blue-400  peer-checked:ring-0 peer-checked:text-white peer-checked:hover:bg-blue-400">The World Wide Web Consortium</div>
+        </label>
         <div>
           <button class="bg-blue-400 text-white p-2 text-lg rounded-md mr-2 w-25 cursor-pointer hover:bg-blue-500">Previous</button>
-          <button class="bg-blue-400 text-white p-2 text-lg rounded-md w-25 cursor-pointer hover:bg-blue-500">Next</button>
+          <RouterLink to="/result">
+            <button class="bg-blue-400 text-white p-2 text-lg rounded-md w-25 cursor-pointer hover:bg-blue-500">Next</button>
+          </RouterLink>
         </div>
       </form>
     </div>
--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
-
+  <div class="flex flex-col items-center mt-20">
+    <div class="flex justify-center flex-col items-center bg-blue-400 w-100 h-100 text-white font-medium rounded-[50%] border-20 border-blue-300">
+      <span class="text-4xl mb-3">Your Score</span>
+      <span class="text-5xl">29/30</span>
+    </div>
+    <span class="text-blue-400 text-5xl font-medium mt-10">Congratulation</span>
+    <span class="mt-3 text-blue-400 text-2xl">Great job! You did it</span>
+    <RouterLink to="/">
+      <button class="bg-blue-400 text-white text-2xl w-100 py-2 rounded-md mt-20 cursor-pointer font-medium hover:bg-blue-500">Back to Home</button>
+    </RouterLink>
+  </div>
 </template>
 
 <style scoped>
