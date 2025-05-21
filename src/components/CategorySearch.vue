2ff4ff724185e006656f95367f08f7dda664181d
chocopie777 <egoran99@gmail.com>
2025-05-21 11:51:02 +0500

    added debounced function in categorySearch

--- src/components/CategorySearch.vue
+++ src/components/CategorySearch.vue
@@ -2,20 +2,21 @@
 import SearchIcon from '@/assets/search-icon.svg';
 import {ref} from "vue";
 import {useCategoryStore} from "@/stores/categoryStore.js";
+import debounce from "@/utils/debounce.js";
 
 const categoryStore = useCategoryStore()
 const search = ref('')
 
-// РЕАЛИЗОВАТЬ ОТЛОЖЕННЫЙ ЗАПРОС К АПИ и начать делать новый проект
 async function categorySearchHandler() {
   await categoryStore.getCategories()
   categoryStore.getCategoriesByName(search.value)
 }
+const debouncedSearch = debounce(categorySearchHandler, 1000);
 </script>
 
 <template>
   <div class="relative mt-5">
-    <input type="text" class="bg-gray-200 w-full p-2 rounded-lg text-xl font-medium pl-5 text-gray-500 placeholder-gray-500 focus:outline-blue-400" placeholder="Search" v-model="search" @input="categorySearchHandler">
+    <input type="text" class="bg-gray-200 w-full p-2 rounded-lg text-xl font-medium pl-5 text-gray-500 placeholder-gray-500 focus:outline-blue-400" placeholder="Search" v-model="search" @input="debouncedSearch">
     <img :src="SearchIcon" alt="search icon" class="w-5 absolute top-[25%] right-5 pointer-events-none">
   </div>
 </template>
